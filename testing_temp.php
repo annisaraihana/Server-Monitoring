c8e5971f28c7fd779658d007415a319480734243
Btari Fatma Rosa <btari.f.r@mail.ugm.ac.id>
2023-01-06 16:21:11 +0700

    last attempt
    Co-Authored-By: annisaraihana <79203499+annisaraihana@users.noreply.github.com>

--- testing_temp.php
+++ testing_temp.php
@@ -41,13 +41,14 @@
         
     </div>
         <div id="small-chart-container">
-            <canvas id="ServerGraph1"></canvas>
+            <canvas id="ServerGraph001"></canvas>
         </div>
     <script>
 
     var ServiceId = [];
     var ServiceName = [];
     var ServiceDesc = [];
+    
 
     $.post("data/GetServiceNames_data.php", function(data)
     {
@@ -58,17 +59,62 @@
             ServiceName.push(data[i].ServiceName);
             ServiceDesc.push(data[i].ServiceDesc);
         }
+        for (var i = 0; i < 32; i++) 
+        { console.log(ServiceId[i]); }
 
-        console.log(ServiceId);
-        console.log(ServiceName);
-        console.log(ServiceDesc);
+        for (var i = 0; i < ServiceId.length; i++){
+
+            $.post("data/ServerStatuses_data.php", { 'ServiceId': ServiceId[i] }, 
+                function (data)
+                {
+                    console.log(data);
+                    var Timestamp = [];
+                    var Status = [];
+                    for (var i in data) {
+                        Timestamp.push(data[i].Timestamp);
+                        Status.push(data[i].status);
+                    }
+                    var chartdata = {
+                        labels: Timestamp,
+                        datasets: [
+                            {
+                                label: ServiceDesc[i], 
+                                backgroundColor: 'rgb(103, 161, 118)',
+                                borderColor: 'rgb(100, 129, 120)',
+                                hoverBackgroundColor: '#CCCCCC',
+                                hoverBorderColor: '#666666',
+                                data: Status,
+                                borderWidth: 1
+                            }
+                        ]
+                    };
+                    var graphTarget = $("#ServerGraph"+ServiceId[i]);
+                    console.log(graphTarget);
+                    var barGraph = new Chart(graphTarget, {
+                        type: 'line',
+                        data: chartdata,    
+                        borderWidth: 1
+                    });
+                });
+
+        }
+
+    });
+
+
+    /*console.log(ServiceId);
+    console.log(ServiceName);
+    console.log(ServiceDesc);
+
+
         /*
         for (var i in ServiceId){
             element = document.createElement('canvas');
             element.setAttribute("id", "ServerGraph" + ServiceId[i]);
         }
-        */
 
+        */
+    /*
         for (var i in ServiceId){
 
             $.post("data/ServerStatuses_data.php", { 'ServiceId': ServiceId[i] }, 
@@ -85,7 +131,7 @@
                         labels: Timestamp,
                         datasets: [
                             {
-                                label: 'ServiceDesc[i]', 
+                                label: ServiceDesc[i], 
                                 backgroundColor: 'rgb(103, 161, 118)',
                                 borderColor: 'rgb(100, 129, 120)',
                                 hoverBackgroundColor: '#CCCCCC',
@@ -95,7 +141,7 @@
                             }
                         ]
                     };
-                    var graphTarget = document.getElementById('ServerGraph1').insertAdjacentHTML(); //UBAH UBAH DISINI IDKKKK
+                    var graphTarget = $("#ServerGraph"+ServiceId[i]);
                     console.log(graphTarget);
                     var barGraph = new Chart(graphTarget, {
                         type: 'line',
@@ -195,7 +241,7 @@
 
             });  
         */
-    });
+   
 
 
     
