a57093d5db7b45db98a49c376c021ddc3602bd6d
Btari Fatma Rosa <btari.f.r@mail.ugm.ac.id>
2023-01-06 09:20:43 +0700

    Added server 011 and 012 status

--- js/DisplayStatusChart.js
+++ js/DisplayStatusChart.js
@@ -402,3 +402,75 @@ function Server010StatusBarChart() {
         });
     }
 }
+
+function Server011StatusBarChart() {
+    {
+        $.post("data/ServerStatuses_data.php", { 'ServiceId': 011 },
+        function (data)
+        {
+            console.log(data);
+            var Timestamp = [];
+            var Status = [];
+            for (var i in data) {
+                Timestamp.push(data[i].Timestamp);
+                Status.push(data[i].status);
+            }
+            var chartdata = {
+                labels: Timestamp,
+                datasets: [
+                    {
+                        label: 'INACBG',
+                        backgroundColor: 'rgb(103, 161, 118)',
+                        borderColor: 'rgb(100, 129, 120)',
+                        hoverBackgroundColor: '#CCCCCC',
+                        hoverBorderColor: '#666666',
+                        data: Status,
+                        borderWidth: 1
+                    }
+                ]
+            };
+            var graphTarget = $("#Server011Graph");
+            var barGraph = new Chart(graphTarget, {
+                type: 'line',
+                data: chartdata,    
+                borderWidth: 1
+            });
+        });
+    }
+}
+
+function Server012StatusBarChart() {
+    {
+        $.post("data/ServerStatuses_data.php", { 'ServiceId': 012 },
+        function (data)
+        {
+            console.log(data);
+            var Timestamp = [];
+            var Status = [];
+            for (var i in data) {
+                Timestamp.push(data[i].Timestamp);
+                Status.push(data[i].status);
+            }
+            var chartdata = {
+                labels: Timestamp,
+                datasets: [
+                    {
+                        label: 'INACBGs Test',
+                        backgroundColor: 'rgb(103, 161, 118)',
+                        borderColor: 'rgb(100, 129, 120)',
+                        hoverBackgroundColor: '#CCCCCC',
+                        hoverBorderColor: '#666666',
+                        data: Status,
+                        borderWidth: 1
+                    }
+                ]
+            };
+            var graphTarget = $("#Server012Graph");
+            var barGraph = new Chart(graphTarget, {
+                type: 'line',
+                data: chartdata,    
+                borderWidth: 1
+            });
+        });
+    }
+}
