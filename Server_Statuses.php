7185e23203a939da5ccc964ec0aa8b0aba485d9a
Btari Fatma Rosa <btari.f.r@mail.ugm.ac.id>
2023-01-11 14:56:14 +0700

    successful but very buggy
    - it won't execute with an external js file.
    - graphs keeps generating on top of each other. Current workaround is adding a 'clear page' button.

--- Server_Statuses.php
+++ Server_Statuses.php
@@ -13,14 +13,25 @@
     <body>
 
     <div class="grid">
+
             <div class="fixed bg-white w-full">
-                <h1 class="text-center font-sans text-2xl font-bold m-4 mb-6">
-                    Grafik Status Server dalam sehari (01 Januari 2023)</h1>
+                <h1 class="text-center font-sans text-2xl font-bold m-4 mb-2">
+                    Grafik Status Server dalam sehari
+                </h1>
+                <div class="ml-8 mb-8">
+                    <p class="font-sans font-bold">
+                    Pilih tanggal : </p>
+                    <p>(Disarankan untuk selalu menekan Clear Page sebelum mengganti tanggal!)</p>
+                    <input type="date" id="dateInput"/>
+                    <button class="text-center text-white font-sans bg-green-500 hover:bg-green-700 ml-4 p-2 rounded" id="Refresh">
+                        Clear Page 
+                    </button>
+                </div>
             </div>
 
-           
-    
-        <div class="grid grid-cols-4 gap-4 mx-4 my-20" id="small-chart-box">
+
+
+        <div class="grid grid-cols-4 gap-4 mx-4 my-48" id="small-chart-box">
 
 
 
@@ -123,6 +134,7 @@
             <div id="small-chart-container">
                 <canvas id="Server032Graph"></canvas>
             </div>
+
         </div>
     </div>
 
@@ -130,9 +142,87 @@
 
     <script>
 
-        $(document).ready(function (){
-            ServerStatusesLineCharts();
+        function PrintDate(date, desc){
+            console.log(date); 
+            console.log(desc);
+        }
+
+        function ServerStatusesLineCharts(input) {
+    
+            console.log(input);
+            var ServiceId = [];
+            var ServiceName = [];
+            var ServiceDesc = [];
+            
+
+            $.post("data/GetServiceNames_data.php", function(data)
+            {
+                console.log(data);
+                
+                for (let i in data) {
+                    ServiceId.push(data[i].ServiceId);
+                    ServiceName.push(data[i].ServiceName);
+                    ServiceDesc.push(data[i].ServiceDesc);
+                }
+
+                for (let i = 0; i < ServiceId.length; i++){
+
+                    $.post("data/ServerStatuses_data.php", { 'ServiceId': ServiceId[i], 'Timestamp': input }, 
+                        function (data)
+                        {
+                            console.log(data);
+                            var Timestamp = [];
+                            var Status = [];
+                            for (let i in data) {
+                                Timestamp.push(data[i].Timestamp);
+                                Status.push(data[i].status);
+                            }
+                            var chartdata = {
+                                labels: Timestamp,
+                                datasets: [
+                                    {
+                                        label: ServiceDesc[i], 
+                                        backgroundColor: 'rgb(103, 161, 118)',
+                                        borderColor: 'rgb(100, 129, 120)',
+                                        hoverBackgroundColor: '#CCCCCC',
+                                        hoverBorderColor: '#666666',
+                                        data: Status,
+                                        borderWidth: 1,
+                                        pointRadius: 1,
+                                        pointBackgroundColor: 'rgb(0, 0, 0)',
+                                        pointBorderColor: 'rgb(0, 0, 0)'
+                                    }
+                                ]
+                            };
+                            var graphTarget = $("#Server"+ServiceId[i]+"Graph");
+                            console.log(graphTarget);
+                            var barGraph = new Chart(graphTarget, {
+                                type: 'line',
+                                data: chartdata,    
+                                borderWidth: 1
+                            });
+                        });
+
+                }
+
+            });
+        }
+
+        var input;
+        var dateEntered;
+
+
+        document.getElementById("Refresh").addEventListener("click", function() {
+            location.reload();
         });
+            
+        document.getElementById("dateInput").addEventListener("change", function() {
+            input = this.value;
+            dateEntered = new Date(input);
+            PrintDate(input, dateEntered);
+            ServerStatusesLineCharts(input);
+        });
+
 
 /*
         function selesai() {
@@ -144,63 +234,9 @@
 */
         </script>
 
-        <script type="text/javascript" src="js\DisplayStatusChart.js" charset="utf-8"></script>
+
    
     <script>
-/*
-    var ServiceId = [];
-    var ServiceName = [];
-    var ServiceDesc = [];
-    
 
-    $.post("data/GetServiceNames_data.php", function(data)
-    {
-        console.log(data);
-        
-        for (let i in data) {
-            ServiceId.push(data[i].ServiceId);
-            ServiceName.push(data[i].ServiceName);
-            ServiceDesc.push(data[i].ServiceDesc);
-        }
-
-        for (let i = 0; i < ServiceId.length; i++){
-
-            $.post("data/ServerStatuses_data.php", { 'ServiceId': ServiceId[i] }, 
-                function (data)
-                {
-                    console.log(data);
-                    var Timestamp = [];
-                    var Status = [];
-                    for (let i in data) {
-                        Timestamp.push(data[i].Timestamp);
-                        Status.push(data[i].status);
-                    }
-                    var chartdata = {
-                        labels: Timestamp,
-                        datasets: [
-                            {
-                                label: ServiceDesc[i], 
-                                backgroundColor: 'rgb(103, 161, 118)',
-                                borderColor: 'rgb(100, 129, 120)',
-                                hoverBackgroundColor: '#CCCCCC',
-                                hoverBorderColor: '#666666',
-                                data: Status,
-                                borderWidth: 1
-                            }
-                        ]
-                    };
-                    var graphTarget = $("#Server"+ServiceId[i]+"Graph");
-                    console.log(graphTarget);
-                    var barGraph = new Chart(graphTarget, {
-                        type: 'line',
-                        data: chartdata,    
-                        borderWidth: 1
-                    });
-                });
-
-        }
-
-    });
-*/
     </script>   
     </body>
