b0cdbef32738f6ac64102808f1a3dd4089f2035d
Btari Fatma Rosa <btari.f.r@mail.ugm.ac.id>
2023-01-12 14:50:22 +0700

    added previous year and current year buttons

--- Yearly_Performance.php
+++ Yearly_Performance.php
@@ -14,8 +14,14 @@
     
     <div class="grid">
             <div class="fixed bg-white w-full">
-                <h1 class="text-center font-sans text-2xl font-bold m-4 mb-6">
-                Performa Tahun ini</h1>
+                <div class="text-center font-sans font-bold m-4 mb-6 mr-20">
+                    <button id="LastYear" class="text-white bg-green-700 font-bold hover:bg-black py-2 px-4 rounded">
+                            Performa tahun lalu  
+                    </button> 
+                    <button id="CurrentYear" class="text-white bg-green-700 font-bold hover:bg-black py-2 px-4 rounded">
+                            Performa Tahun ini
+                    </button>    
+                </div>
             </div> 
 
         <div class="grid grid-cols-4 gap-4 mx-4 my-20" id="small-chart-box">
@@ -122,7 +128,12 @@
     <script>
 
         $(document).ready(function (){
-            ServerPercentageCurrentYear();
+            document.getElementById('CurrentYear').onclick=function(){
+                ServerPercentageCurrentYear();
+            };
+            document.getElementById('LastYear').onclick=function(){
+                ServerPercentagePrevYear();
+            };
         });
 
         </script>
@@ -130,95 +141,7 @@
 
 
     <script>
-/*
-    var ServiceId = [];
-    var ServiceName = [];
-    var ServiceDesc = [];
-    
 
-    $.post("data/GetServiceNames_data.php", function(data)
-    {
-        console.log(data);
-        
-        for (let i in data) {
-            ServiceId.push(data[i].ServiceId);
-            ServiceName.push(data[i].ServiceName);
-            ServiceDesc.push(data[i].ServiceDesc);
-        }
-
-        for (let i = 0; i < ServiceId.length; i++){
-
-            $.post("data/ServerPercentageYearly_data.php", { 'ServiceId': ServiceId[i] }, 
-                function (data)
-                {
-                    console.log(data);
-
-                    var active = [];
-                    var inactive = [];
-
-                    for (let i in data) {
-                        active.push(data[i].ActiveCount);
-                        inactive.push(data[i].InactiveCount);
-                    }
-
-                    var chartdata = {
-                        labels: ["active","inactive"],
-                        datasets: [
-                            {
-                                label: 'Active percentage',
-                                backgroundColor: '#49e2ff',
-                                borderColor: '#46d5f1',
-                                hoverBackgroundColor: '#CCCCCC',
-                                hoverBorderColor: '#666666',
-                                data: [active,inactive],
-                                backgroundColor: [
-                                    'rgba(255, 99, 132, 0.2)',
-                                    'rgba(54, 162, 235, 0.2)',
-                                    
-                                ],
-                                borderColor: [
-                                    'rgba(255,99,132,1)',
-                                    'rgba(54, 162, 235, 1)',
-                                
-                                ],
-                                borderWidth: 1
-                            }
-                        ]
-                    };
-                    
-                    var graphTarget = $("#Server"+ServiceId[i]+"Percentage");
-                    console.log(graphTarget);
-                    var barGraph = new Chart(graphTarget, {
-                    type: 'doughnut',
-                    data: chartdata,
-                    options: {
-                        cutoutPercentage: 80,
-                        title: {
-                        display: true,
-                        text: ServiceDesc[i]
-                        },
-                        plugins: {
-                            doughnutlabel: {
-                                labels: [
-                                {
-                                    text: (parseInt(active)*100/(parseInt(active)+parseInt(inactive))).toFixed(3)+'%',
-                                    font: {
-                                    size: '20'
-                                    }
-                                }
-                                
-                                ]
-                            }
-
-                        }
-                    }
-                });
-                });
-
-        }
-
-    });
-*/
 
     </script>   
     </body>
--- js/DisplayPercentagesYear.js
+++ js/DisplayPercentagesYear.js
@@ -84,3 +84,90 @@ function ServerPercentageCurrentYear() {
     });
 
 }
+
+function ServerPercentagePrevYear() {
+    
+    var ServiceId = [];
+    var ServiceName = [];
+    var ServiceDesc = [];
+    
+
+    $.post("data/GetServiceNames_data.php", function(data)
+    {
+        console.log(data);
+        
+        for (let i in data) {
+            ServiceId.push(data[i].ServiceId);
+            ServiceName.push(data[i].ServiceName);
+            ServiceDesc.push(data[i].ServiceDesc);
+        }
+
+        for (let i = 0; i < ServiceId.length; i++){
+
+            $.post("data/ServerPercentagePrevYear_data.php", { 'ServiceId': ServiceId[i] }, 
+                function (data)
+                {
+                    console.log(data);
+
+                    var active = [];
+                    var inactive = [];
+
+                    for (let i in data) {
+                        active.push(data[i].ActiveCount);
+                        inactive.push(data[i].InactiveCount);
+                    }
+
+                    var chartdata = {
+                        labels: ["active","inactive"],
+                        datasets: [
+                            {
+                                data: [active,inactive],
+                                backgroundColor: [
+                                    'rgba(52, 211, 153, 0.7)',
+                                    'rgba(253, 224, 71, 0.7)',
+
+                                ],
+                                borderColor: [
+                                    'rgba(16, 185, 129, 1)',
+                                    'rgba(250, 204, 21,1)',
+                                
+                                ],
+                                borderWidth: 1
+                            }
+                        ]
+                    };
+                    
+                    var graphTarget = $("#Server"+ServiceId[i]+"Percentage");
+                    console.log(graphTarget);
+                    var barGraph = new Chart(graphTarget, {
+                    type: 'doughnut',
+                    data: chartdata,
+                    options: {
+                        cutoutPercentage: 80,
+                        title: {
+                        display: true,
+                        text: ServiceDesc[i]
+                        },
+                        plugins: {
+                            doughnutlabel: {
+                                labels: [
+                                {
+                                    text: (parseInt(active)*100/(parseInt(active)+parseInt(inactive))).toFixed(3)+'%',
+                                    font: {
+                                    size: '20'
+                                    }
+                                }
+                                
+                                ]
+                            }
+
+                        }
+                    }
+                });
+                });
+
+        }
+
+    });
+
+}
