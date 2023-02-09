bd199d763c7ba2d9b333902456a47368c3a2fff6
BetNares <btari.f.r@mail.ugm.ac.id>
2023-02-09 15:14:04 +0700

    fix query to using prepared statements

--- data/ServerStatuses_data.php
+++ data/ServerStatuses_data.php
@@ -4,17 +4,28 @@ include '../env.php';
 include '../autoload.php';
 
 header('Content-Type: application/json');
-$ServiceId = $_POST['ServiceId'];
-$Timestamp = $_POST['Timestamp'];
+
 $conn = mysqli_connect(env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'),env('DB_NAME'));
-$sqlQuery = "SELECT trans_status.run_time AS Timestamp, trans_status.status FROM trans_status WHERE trans_status.idservice =  " . $ServiceId . "  AND `trans_status`.`run_time` >= '". $Timestamp ." 00:00:01' AND `trans_status`.`run_time` <= '". $Timestamp ." 23:59:59';";
-$result = mysqli_query($conn,$sqlQuery);
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+  }
+
+$sqlQuery = "SELECT trans_status.run_time AS Timestamp, trans_status.status FROM trans_status WHERE trans_status.idservice = ? AND `trans_status`.`run_time` >= ? AND `trans_status`.`run_time` <= ? ;";
+$stmt = $conn->prepare($sqlQuery); 
+$stmt->bind_param("iss", $ServiceId, $TimestampStart, $TimestampEnd);
+
+$ServiceId = $_POST['ServiceId'];
+$TimestampStart = $_POST['Timestamp'] . " 00:00:01";
+$TimestampEnd = $_POST['Timestamp'] . " 23:59:59";
+$stmt->execute();
+
+$result = $stmt->get_result();
 $data = array();
-foreach ($result as $row) {
+while ($row = $result->fetch_assoc()) {
     $data[] = $row;
 }
 mysqli_close($conn);
 echo json_encode($data);
-
+$stmt->close();
 
 ?>
